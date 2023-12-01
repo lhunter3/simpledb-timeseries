2f1c5adc96d9e744363ad446a67ddc641946a493
Lucas Hunter <lucasbhunter@gmail.com>
2023-12-01 17:41:24 -0400

    cleanup

--- src/simpledb/query/TimeseriesConstant.java
+++ src/simpledb/query/TimeseriesConstant.java
@@ -11,62 +11,74 @@ public class TimeseriesConstant implements Constant, Serializable {
    
    /**
     * Create a constant by wrapping the specified data array.
-    * @param value - Long[] array of {value, date}
+    * @param value - int[] array of {value, date}
     */
    public TimeseriesConstant(int[] arr) {
       this.value = arr[0];
       this.trs = arr[1];
    }
 
-  
    
    /**
-    * Unwraps the object and returns a formated  string. [yyyy-MM-dd HH:mm:ss  value]
-    * @see simpledb.query.Constant#asJavaVal()
+    * Unwraps and returns the object
+    * @return Object
     */
    public Object asJavaVal() {
       return this;
    }
 
+   /**
+    * Gets the Integer value of the TimeseriesConstant
+    * @return IntConstant
+    */
    public IntConstant geIntConstant(){
       return new IntConstant(this.value);
    }
 
+   /**
+    * Gets the TRS Time value from the TimeseriesConstant as IntConstant
+    * @return IntConstant
+    */
    public IntConstant getTrsConstant(){
       return new IntConstant(this.trs);
    }
    
+   /**
+    * Gets the TRS Time value from the TimeseriesConstant as StringConstant
+    * @return StringConstant
+    */
    public StringConstant getTrsStringConstant(){
       return new StringConstant(this.trsToString());
    }
+
 
    public boolean equals(Object obj) {
          TimeseriesConstant t =  (TimeseriesConstant) obj;
          return t != null && this == t;
    }
 
-
    public int compareTo(Constant obj) {
       TimeseriesConstant t = (TimeseriesConstant) obj;
       return this.trs.compareTo(t.trs);
    }
 
+ 
    public int hashCode() {
       return Float.hashCode(value);
    }
-   
-   public String toString() {
-      return  "["+ this.trsToString()  +"\t" + this.value + "]";
-   }
-
+ 
+   /**
+    * Converts the TRS Time value from the TimeseriesConstant to a readable String
+    * @return String
+    */
    public String trsToString() { 
-       long millis = this.trs * 60 * 60 * 1000L;
-
-      // Create a Date object based on the milliseconds from epoch
+      long millis = this.trs * 60 * 60 * 1000L;
       Date date = new Date(millis);
-
-      // Create a SimpleDateFormat to format the date
       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH");
       return sdf.format(date);
+   }
+
+   public String toString() {
+      return  "["+ this.trsToString()  +"\t" + this.value + "]";
    }
 }
